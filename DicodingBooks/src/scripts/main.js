807b4850ff265e5e7fcb208118b95387f9aca051
wynprtm <wayanpratama38@gmail.com>
2025-04-14 02:41:47 +0700

    Removing XMLHTTPRequest method in updatebook function

--- DicodingBooks/src/scripts/main.js
+++ DicodingBooks/src/scripts/main.js
@@ -62,26 +62,6 @@ function main() {
       .catch((error) => {
         showResponseMessage(error);
       })
-
-    const xhr = new XMLHttpRequest();
-
-    xhr.onload = function () {
-      const responseJson = JSON.parse(this.responseText);
-      showResponseMessage(responseJson.message);
-      getBook();
-    }
-
-    xhr.onerror = function () {
-      showResponseMessage();
-    }
-
-    xhr.open("PUT",`${baseUrl}/edit/${book.id}`);
-
-    xhr.setRequestHeader('Content-Type','application/json');
-    xhr.setRequestHeader('X-Auth-Token','12345');
-
-    xhr.send(JSON.stringify(book));
-
   };
 
   const removeBook = (bookId) => {
